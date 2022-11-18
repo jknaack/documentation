e3ba026d8e7a051b91d43665e91b8fcd85e0c5e4
ludeeus <ludeeus@ludeeus.dev>
2022-11-18 10:23:19 +0000

    dynamic site_url

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,25 @@
-const yaml = require("js-yaml")
+const yaml = require('js-yaml')
+
+const FALLBACK_BASE_URL = 'https://hacs.xyz'
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ "src/_static": "static" });
-    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
-      
-    return {
-      dir: {
-        input: "src",
-        output: "dist",
-        layouts: "_layouts",
-        data: "_data",
-        htmlTemplateEngine: "liquid",
-      },
-    };
-  };
+  eleventyConfig.addPassthroughCopy({ 'src/_static': 'static' })
+  eleventyConfig.addDataExtension('yaml', (contents) => ({
+    ...yaml.load(contents),
+    site_url: !process.env.CF_PAGES_BRANCH
+      ? 'http://localhost:8080/'
+      : process.env.CF_PAGES_BRANCH === 'main' || !process.env.CF_PAGES_URL
+      ? FALLBACK_BASE_URL
+      : process.env.CF_PAGES_URL,
+  }))
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      layouts: '_layouts',
+      data: '_data',
+      htmlTemplateEngine: 'liquid',
+    },
+  }
+}
