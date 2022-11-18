e599d96b48421c0a27cc5d220cd028f5e4547a90
ludeeus <ludeeus@ludeeus.dev>
2022-11-18 09:17:41 +0000

    config an mobile

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
-
+const yaml = require("js-yaml")
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "src/_static": "static" });
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
       
     return {
       dir: {
         input: "src",
         output: "dist",
         layouts: "_layouts",
+        data: "_data",
         htmlTemplateEngine: "liquid",
       },
     };
