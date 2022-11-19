d5a8af7a2b264855c6a068ffeea51e4e496a3a27
ludeeus <ludeeus@ludeeus.dev>
2022-11-19 11:46:57 +0000

    Mobile nav

---
+++ src/_static/openMobileNav.js
@@ -0,0 +1,4 @@
+function openMobileNav() {
+    const mobileNavigationOverlay = document.querySelector(".mobile-navigation-overlay")
+    mobileNavigationOverlay.style.display = mobileNavigationOverlay.style.display === "block" ? "none" : "block" 
+}
